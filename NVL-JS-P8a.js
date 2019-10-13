2b84f3c8c6ffd754dc4b929c715d936ec1f61d69
Tachikoma82 <losdomingosdejuano@gmail.com>
2019-10-13 21:13:39 +0200

    Tres maneras de cambiar el valor de una cadena de texto

---
+++ NVL-JS-P8a.js
@@ -0,0 +1,18 @@
+var amigo = "Amigo";
+//variable original
+
+//forma #1
+alert(amigo.toLowerCase());
+// pone todo el texto en minúscula
+
+
+
+//forma #2
+alert(amigo.replace(“A”, “a”));
+// pone la "a" en minúscula
+
+
+
+//forma #3
+var amigo = "amigo";
+// sobreescribe el valor de la variable con un nuevo nombre, esta vez todo en minúscula
